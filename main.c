e530be99eaf9f473cf8e2ba296c3f16910a69612
Isaben <isabenjuda@gmail.com>
2017-05-27 21:04:14 -0300

    esqueci do srand

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "honeypot.h"
+#include <stdlib.h>
+#include <time.h>
 
 int main(){
-
+    srand(time(NULL));
     printf("SEQUENCIALMENTE: %d segundos", examine_servers_sequentially());
     
     return 0;
