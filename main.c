e5bbc841479ca58e308d25701a46758472575d82
Dave <oblivion.davecave@gmail.com>
2017-05-29 12:51:51 +0000

    Organização de alguns comentários

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 
 #define MALICIOUS 1
 #define BENIGN 0
-#define WAIT_TIME 15 // 15 segundos, simular tempo de classificação. Tw do artigo
-#define RESPONSE_TIME 5 // 5 segundos, simular tempo de resposta dos servidores. Ti do artigo
-#define RESET_TIME 10 // 10 segundos, simular tempo de reset do client. Tr do artigo
-#define MAX_SERVERS 50 // quantidade de servidores
-#define PROB_MALICIOUS 5 // em porcentagem e.g se 20, significa 20% de chance de um servidor ser malicioso. Utilizado na init_servers()
+#define WAIT_TIME 15 // Tempo de classificação em segundos. Tw do artigo
+#define RESPONSE_TIME 5 // Tempo de resposta dos servidores em segundos. Ti do artigo
+#define RESET_TIME 10 // Tempo para reiniciar o servidor ao estado limpo em segundos. Tr do artigo
+#define MAX_SERVERS 50 // Quantidade de servidores
+#define PROB_MALICIOUS 5 // Probabilidade de servidores maliciosos (0 a 100)
 
-// retorna um vetor de inteiros, cada índice correspondendo a um servidor
+/*  Retorna um vetor de inteiros, cada índice correspondendo a um servidor
+    Cada elemento do vetor possui uma flag BENIGN ou MALICIOUS
+*/
 int* init_servers(){
     int* servers = (int*) malloc(MAX_SERVERS * sizeof(int));
     int i;
@@ -26,7 +28,9 @@ int* init_servers(){
     return servers;
 }
 
-// retorna o tempo necessário para examinar de acordo com os parâmetros
+/*  Retorna o tempo necessário para examinar os servidores usando a estratégia
+    sequencial, de acordo com os parâmetros definidos nas macros.
+*/
 int examine_servers_sequentially(){
     int total_time = 0;
     int* servers = init_servers();
@@ -42,7 +46,8 @@ int examine_servers_sequentially(){
     return total_time;
 }
 
-// examina os servidores recursivamente, armazena o tempo no ponteiro total_time
+/* Examina os servidores recursivamente, armazena o tempo no ponteiro total_time
+*/
 void examine_servers_recursively(int* servers, int* total_time, int start, int end) {
 
     if(start < end - 1) {
@@ -65,7 +70,9 @@ void examine_servers_recursively(int* servers, int* total_time, int start, int e
     }
 }
 
-// retorna o tempo necessário para examinar os servidores usando divisão e conquista
+/*  Retorna o tempo necessário para examinar os servidores usando a estratégia
+    de divisão e conquista, de acordo com os parâmetros definidos nas macros.
+*/
 int divide_and_conquer_servers(){
 
     int* servers = init_servers();
